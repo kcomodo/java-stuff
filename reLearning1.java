feab7884d8e88fe46b38d15f653ab3d6f10acee9
Quang Ho <mxrshmalloww@gmail.com>
2020-02-27 09:10:51 -0500

    Add files via upload

---
+++ reLearning1.java
@@ -0,0 +1,13 @@
+
+public class reLearning1 {
+	public static void main(String[] args) {
+		System.out.print("Hello world");
+		int numberThingy = 5;
+		int i = 1;
+		while(i <= numberThingy)
+			System.out.println("Line: "+i);
+			i+=1;
+
+	}
+
+}
